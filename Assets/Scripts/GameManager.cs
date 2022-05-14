98bf293e0bd2a31a0cc8126c44ccd6ee6adb0530
rpucadyi <rahul.pucadyil@stud.hslu.ch>
2022-05-14 10:01:21 +0200

    First

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    [SerializeField] List<string> mSceneNames;
+
+    [SerializeField] string mCurrentSceneName;
+
+    PlayerController mPlayerControllerReference;
+    private void Awake() {
+        DontDestroyOnLoad(this);
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        mPlayerControllerReference = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
